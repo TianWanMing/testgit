41fd7c05ca493dc211cb07c49946574acbfb6e4d
twm <1472928954@qq.com>
2020-04-04 23:26:11 +0800

    测试

--- src/main/java/com/taotao/controller/index.java
+++ src/main/java/com/taotao/controller/index.java
@@ -22,6 +22,7 @@ public class index {
 	@RequestMapping("/updata")
 	public String name() {
 		System.out.println("---+++++33333333333333333+-----");
+		System.out.print("顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶");
 		System.out.println("---++++++---2222222222222222--");
 		return "uploand";
 	}
